d23b816015a4e5bc386da88ec6328a60d1cad6ba
Pierre-Yves Le Borgne <pierre-yves.le-borgne@epitech.eu>
2012-10-20 07:25:04 +0800

    more functions added to grammar

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -20,10 +20,16 @@ public:
 
     new_line_ = lit('\n') | ':';
 
+    abs_ = lit("Abs ") >> numeric_rvalue_;
+
+    and_ = lit(" And ");
+
     locate_ = lit("Locate ")
       >> numeric_rvalue_ >> ','
       >> numeric_rvalue_ >> ",\""
       >> *(~char_('"')) >> '"';
+
+    comment_ = '\'' >> *(char_ - new_line_);
 
     condition_if_ = lit("If ")
       >> numeric_rvalue_ >> new_line_
@@ -40,6 +46,11 @@ public:
       >> *(!(lit("WhileEnd")) >> expression_ >> +new_line_)
       >> lit("WhileEnd");
 
+    condition_do_lpwhile_ = lit("Do") >> new_line_
+		>> *(!(lit("LpWhile")) >> expression_ >> +new_line_)
+		>> lit("LpWhile ") >> numeric_rvalue_;
+
+    or_ = lit(" Or ");
 
     list_index_ = int_;
     
@@ -51,12 +62,33 @@ public:
 
     numeric_lvalue_ = (list_rvalue_ >> '[' >> numeric_rvalue_ >> ']') | variable_;
 
-    numeric_rvalue_ = int_ | numeric_lvalue_;
-
-    void_expression_ = locate_ | condition_if_;
-
-    expression_ = void_expression_ | list_rvalue_ | matrix_rvalue_ | numeric_rvalue_
+    operator_binary_ = operator_gt_
+      | operator_lt_
+      | operator_eq_
+      | operator_neq_
+      | and_
+      | or_
+      | char_("-+*/")
       ;
+    operator_unary_ = char_("-+");
+
+    // doesnt work yet xD
+    numeric_rvalue_ = *(char_('(') | operator_unary_) >> (int_ | abs_) >> *char_(')') >> -(operator_binary_ >> numeric_lvalue_);
+
+    void_expression_ =
+      locate_
+      | condition_if_
+      | condition_while_
+      | condition_do_lpwhile_
+      // | 
+      ;
+
+    operator_gt_ = lit('>');
+    operator_lt_ = lit('<');
+    operator_eq_ = lit('=');
+    operator_neq_ = lit("!=");
+
+    expression_ = void_expression_ | list_rvalue_ | matrix_rvalue_ | numeric_rvalue_;
 
     start_ %= *(expression_ >> +new_line_);
   }
@@ -70,7 +102,46 @@ private:
 
   boost::spirit::qi::rule<Iterator> new_line_;
 
-  boost::spirit::qi::rule<Iterator> temp;
+  boost::spirit::qi::rule<Iterator> comment_;
+
+  boost::spirit::qi::rule<Iterator> operator_unary_;
+  boost::spirit::qi::rule<Iterator> operator_binary_;
+
+  boost::spirit::qi::rule<Iterator> locate_;
+  boost::spirit::qi::rule<Iterator> condition_if_;
+  boost::spirit::qi::rule<Iterator> condition_while_;
+  boost::spirit::qi::rule<Iterator> condition_do_lpwhile_;
+
+  boost::spirit::qi::rule<Iterator> interrogation_mark_;
+  boost::spirit::qi::rule<Iterator> abs_;
+  boost::spirit::qi::rule<Iterator> and_;
+  boost::spirit::qi::rule<Iterator> ans_;
+  boost::spirit::qi::rule<Iterator> augment_;
+
+  boost::spirit::qi::rule<Iterator> axesoff_;
+  boost::spirit::qi::rule<Iterator> axeson_;
+  boost::spirit::qi::rule<Iterator> bg_none_;
+  boost::spirit::qi::rule<Iterator> bg_pict_;
+  boost::spirit::qi::rule<Iterator> break_;
+  boost::spirit::qi::rule<Iterator> circle_;
+  boost::spirit::qi::rule<Iterator> clrgraph_;
+  boost::spirit::qi::rule<Iterator> clrlist_;
+  boost::spirit::qi::rule<Iterator> clrmat_;
+  boost::spirit::qi::rule<Iterator> clrtext_;
+  boost::spirit::qi::rule<Iterator> cls_;
+  boost::spirit::qi::rule<Iterator> coordoff_;
+  boost::spirit::qi::rule<Iterator> coordon_;
+  boost::spirit::qi::rule<Iterator> cross_;
+
+  boost::spirit::qi::rule<Iterator> or_;
+
+
+  boost::spirit::qi::rule<Iterator> operator_gt_;
+  boost::spirit::qi::rule<Iterator> operator_lt_;
+  boost::spirit::qi::rule<Iterator> operator_eq_;
+  boost::spirit::qi::rule<Iterator> operator_neq_;
+
+
 
   boost::spirit::qi::rule<Iterator> void_expression_;
   boost::spirit::qi::rule<Iterator> numeric_lvalue_;
@@ -86,10 +157,6 @@ private:
   boost::spirit::qi::rule<Iterator> variable_;
   boost::spirit::qi::rule<Iterator> list_;
   boost::spirit::qi::rule<Iterator> matrix_;
-
-  boost::spirit::qi::rule<Iterator> locate_;
-  boost::spirit::qi::rule<Iterator> condition_if_;
-  boost::spirit::qi::rule<Iterator> condition_while_;
 };
 
 #endif	// !PARSER_HPP_
